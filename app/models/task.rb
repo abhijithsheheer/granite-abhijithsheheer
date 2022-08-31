6e3f3d0564260deee4d69a1a99b3c9eb8c6859cf
Abhijith Sheheer <89389135+abhijithsheheer@users.noreply.github.com>
2022-08-31 20:35:31 +0530

    Fixed order of items in Active Record models

--- app/models/task.rb
+++ app/models/task.rb
@@ -5,8 +5,10 @@ class Task < ApplicationRecord
 
   enum status: { unstarred: "unstarred", starred: "starred" }
   enum progress: { pending: "pending", completed: "completed" }
+
   belongs_to :task_owner, foreign_key: "task_owner_id", class_name: "User"
   belongs_to :assigned_user, foreign_key: "assigned_user_id", class_name: "User"
+
   validates :title, presence: true, length: { maximum: 50 }
   validates :slug, uniqueness: true
   validate :slug_not_changed
